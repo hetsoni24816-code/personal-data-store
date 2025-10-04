e3ae174d850f4861e3ca348e6181ad0df95e63a8
Het Soni <hetsoni1248@gmail.com>
2025-10-04 14:38:50 +1300

    Remove Org Requests page

--- pages/4_OrgRequests.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-# Require login before page loads
-if not st.session_state.get("uid"):
-    st.warning("Please log in first (see **Login / Sign Up** page).")
-    st.stop()
